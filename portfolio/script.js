1cc9fe0f0b7fc34f0328d5a4990ff14725e5d0fe
Sampie1 <101374@glr.nl>
2025-12-07 01:49:48 +0100

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,17 @@
+ScrollReveal().reveal('.sec-skills', {
+    origin: 'left',     
+    distance: '50px',   
+    duration: 1000,     
+    delay: 200,         
+    easing: 'ease-in-out',
+    reset: false        
+  });
+
+  ScrollReveal().reveal('.sec-projecten', {
+    origin: 'bottom',     
+    distance: '50px',   
+    duration: 1000,     
+    delay: 200,         
+    easing: 'ease-in-out',
+    reset: false        
+  });
